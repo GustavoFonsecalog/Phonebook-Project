cf00dedc2953ff36f7d14fc84f46fe0bab3c0ff5
GustavoFonsecalog <gustavofonsecajb@hotmail.com>
2023-03-23 20:04:45 -0300

    Put funcional e ajustes de bugs

--- src/routes/updateUser.tsx
+++ src/routes/updateUser.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import scheduleFetch from "../axios/config";
 import "../css/newUser.css";
@@ -6,21 +6,102 @@ import { Contact } from "../interface/contact";
 
 import { useNavigate, useParams } from "react-router-dom";
 
+interface PhoneNumber {
+  id: number;
+  id_schedule: number;
+  number: number;
+}
+
 const UpdateUser = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const { register, handleSubmit, reset } = useForm({});
+  const { register, handleSubmit, reset, getValues, setValue } = useForm({});
+  const [localNumbers, setLocalNumbers] = useState<PhoneNumber[]>();
+
+  console.log(localNumbers);
+
+  const resetAsyncForm = useCallback(async () => {
+    scheduleFetch.get(`/schedule/${id}`).then((response) => {
+      const { name, id, date_born, cpf, email, numbers } =
+        response.data.data[0];
+      reset({
+        name: name,
+        id: id,
+        date_born: date_born,
+        cpf: cpf,
+        email: email,
+        numbers: numbers,
+      });
+      setLocalNumbers(
+        numbers
+          .map((item: any) => item)
+          .sort((a: PhoneNumber, b: PhoneNumber) => a.id - b.id)
+      );
+    });
+  }, [reset]);
 
   useEffect(() => {
-    scheduleFetch.get(`/schedule/${id}`).then((response) => {
-      console.log(response.data.data);
-      reset(response.data.data);
-    });
-  }, []);
+    resetAsyncForm();
+  }, [resetAsyncForm]);
+
+  useEffect(() => {
+    setValue(
+      "numbers",
+      localNumbers?.map((item) => String(item.number))
+    );
+  }, [localNumbers]);
+
+  const hasNumbers = getValues("numbers");
 
   const onSubmit = async () => {
-    scheduleFetch.put(`/schedule/edit/${id}`);
+    console.log(getValues("name"));
+    scheduleFetch.put(`/schedule/${id}`, {
+      name: getValues("name"),
+      cpf: getValues("cpf"),
+      email: getValues("email"),
+      date_born: getValues("date_born"),
+      numbers: getValues("numbers"),
+    });
+  };
+
+  const handleChangeNumber = (event: any, number: PhoneNumber) => {
+    event.preventDefault();
+
+    const inputValue = event.target.value;
+
+    if (!localNumbers || localNumbers.length === 0) return;
+
+    const editedNumber = localNumbers.find(
+      (item: PhoneNumber) => item.id === number.id
+    );
+
+    if (!editedNumber) return;
+
+    const currentFiltered = localNumbers.filter(
+      (item: PhoneNumber) => item.id !== number.id
+    );
+
+    if (!currentFiltered) {
+      setLocalNumbers(
+        [
+          {
+            ...editedNumber,
+            number: inputValue,
+          },
+        ].sort((a: PhoneNumber, b: PhoneNumber) => a.id - b.id)
+      );
+    }
+
+    setLocalNumbers(
+      [
+        ...currentFiltered,
+        {
+          ...editedNumber,
+          number: inputValue,
+        },
+      ].sort((a: PhoneNumber, b: PhoneNumber) => a.id - b.id)
+    );
   };
 
   return (
@@ -47,14 +128,16 @@ const UpdateUser = () => {
       <br />
       <label className="labelForm">
         Telefones:
-        {Array.from({ length: 3 }).map((_, index) => (
-          <input
-            className="inputForm"
-            type="tel"
-            key={index}
-            {...register(`numbers.${index}`)}
-          />
-        ))}
+        {localNumbers &&
+          localNumbers.map((number: PhoneNumber) => (
+            <input
+              className="inputForm"
+              type="tel"
+              key={number.id}
+              onChange={(e: any) => handleChangeNumber(e, number)}
+              value={number.number}
+            />
+          ))}
       </label>
       <br />
       <button type="submit" className="buttonForm">
